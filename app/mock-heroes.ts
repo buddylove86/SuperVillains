e87d3fb6b81a524eef103e3b5113fa05fa00e0fb
Marcus Love <love.m.d1974@gmail.com>
2017-11-20 20:57:43 +1300

    Add files via upload

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+	{id: 11, name: 'Mr. Nice' },
+	{id: 12, name: 'Narco' },
+	{id: 13, name: 'Bombasto' },
+	{id: 14, name: 'Magneta' },
+	{id: 15, name: 'Rubber man'	},
+	{id: 16, name: 'Dynama'	},
+	{id: 17, name: 'Dr IQ' },
+	{id: 18, name: 'Magma' },
+	{id: 19, name: 'Tornado' },
+	{id: 20, name: 'Celeritas' }
+	];
